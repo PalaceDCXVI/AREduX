80ff814a8ef6cdce7495b34a80ba938c7210b4f8
Claire Culver <ClaireMCulver@gmail.com>
2022-06-14 21:45:56 -0400

    Initial OpenVisSim commit
    Added OpenVisSim and initial scene that uses an effect, the EyeTrackingTest scene.

---
+++ Assets/Scripts/OpenVisSim/PlayMovieOnSpace.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
+
+public class PlayMovieOnSpace : MonoBehaviour
+{
+    public UnityEngine.Video.VideoClip videoClip;
+    public VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        var videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        var audioSource = gameObject.AddComponent<AudioSource>();
+
+        videoPlayer.playOnAwake = false;
+        videoPlayer.clip = videoClip;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+    }
+    void Update()
+    {
+        // MovieTexture doesn't work on webgl or phones (?)
+#if UNITY_EDITOR
+        if (Input.GetButtonDown("Jump"))
+        {
+            videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+            videoPlayer.targetMaterialProperty = videoPlayer.targetMaterialRenderer.material.mainTexture.name;
+
+            if (videoPlayer.isPlaying)
+            {
+                videoPlayer.Pause();
+            }
+            else {
+                videoPlayer.Play();
+            }
+        }
+#endif
+    }
+}
