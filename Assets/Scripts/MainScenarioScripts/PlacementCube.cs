37d7dc59ca16f3dd15e3551ff71ab1e824b407cb
Claire Culver <ClaireMCulver@gmail.com>
2022-11-16 13:53:43 -0500

    changes to placement indicators

---
+++ Assets/Scripts/MainScenarioScripts/PlacementCube.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementCube : MonoBehaviour
+{
+    public float StartingCountdown = 5.0f;
+    private float countdown;
+
+    bool CountingDown = false;
+
+
+    public float minAlpha = 0.0f;
+    public float maxAlpha = 0.2f;
+    public float timeDistortion = 0.1f;
+    public float alphaChangeDirection = 1.0f;
+    private float alphaValue = -0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = StartingCountdown;
+
+        Color currentColour = GetComponent<MeshRenderer>().material.color;
+        currentColour.a = 0.0f;
+
+        GetComponent<MeshRenderer>().material.color = currentColour;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CountingDown)
+        {
+            countdown -= Time.deltaTime;
+
+            if (countdown < 0.0f)
+            {
+                if (alphaValue < minAlpha)
+                {
+                    alphaChangeDirection = 1.0f;
+                }
+                else if (alphaValue > maxAlpha)
+                {
+                    alphaChangeDirection = -1.0f;
+                }
+
+                float alphaChange = Time.deltaTime * timeDistortion * alphaChangeDirection;
+
+                alphaValue += alphaChange;
+
+                Color currentColour = GetComponent<MeshRenderer>().material.color;
+                currentColour.a = Mathf.Clamp01(alphaValue);
+
+                GetComponent<MeshRenderer>().material.color = currentColour;
+            }
+        }
+    }
+
+    public void StartCountdown()
+    {
+        CountingDown = true;
+    }
+
+    public void RestartCountdown()
+    {
+        CountingDown = false;
+        countdown = StartingCountdown;
+        alphaChangeDirection = 1.0f;
+        alphaValue = -0.1f;        
+        Color currentColour = GetComponent<MeshRenderer>().material.color;
+        currentColour.a = 0.0f;
+
+        GetComponent<MeshRenderer>().material.color = currentColour;
+    }
+}
