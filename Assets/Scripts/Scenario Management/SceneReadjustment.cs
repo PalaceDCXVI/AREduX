c93748bb4e1474c644a4afbdba2683de7b01d013
Claire <ClaireCoding@gmail.com>
2022-12-19 18:48:48 -0500

    Scene reorient functionality

--- Assets/Scripts/Scenario Management/SceneReadjustment.cs
+++ Assets/Scripts/Scenario Management/SceneReadjustment.cs
@@ -8,10 +8,11 @@ using UnityEngine.InputSystem;
 
 public class SceneReadjustment : MonoBehaviour
 {
+    public GameObject SceneRoot;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -22,31 +23,9 @@ public class SceneReadjustment : MonoBehaviour
 
     public void RecenterScene()
     {
-        if (XRGeneralSettings.Instance.Manager.activeLoaders.Count > 0)
-        {
-            XRInputSubsystem xRInputSubsystem = XRSubsystemHelpers.InputSubsystem;
-            bool? success = xRInputSubsystem?.TryRecenter();
-
-            XRLoader loader = XRGeneralSettings.Instance.Manager.activeLoaders[0];
-            XRGeneralSettings.Instance.Manager.activeLoaders[0].Initialize();
-            XRGeneralSettings.Instance.Manager.activeLoaders[0].Start();
-            UnityEngine.XR.XRInputSubsystem inputSubsystem = XRGeneralSettings.Instance.Manager.activeLoaders[0].GetLoadedSubsystem<UnityEngine.XR.XRInputSubsystem>();
-            Debug.Log("Input feteched");
-            if (inputSubsystem != null)
-            {
-                inputSubsystem.TryRecenter();
-                Debug.Log("Recenter attempted");
-            }
-        }
-
-
-        List<ISubsystem> list = new List<ISubsystem>();
-        SubsystemManager.GetInstances(list);
-        List<UnityEngine.XR.XRInputSubsystem> xRInputSubsystems = new List<XRInputSubsystem>();
-        SubsystemManager.GetInstances<UnityEngine.XR.XRInputSubsystem>(xRInputSubsystems);
-        if (xRInputSubsystems.Count > 0)
-        {
-            xRInputSubsystems[0].TryRecenter();
-        }
+        Vector3 repositionVector = Camera.main.transform.position - SceneRoot.transform.position;
+        repositionVector.x = 0; repositionVector.z = 0;
+        SceneRoot.transform.Translate(repositionVector);
+        Camera.main.transform.Translate(-repositionVector);
     }
 }
